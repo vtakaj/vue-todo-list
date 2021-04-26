2d68d7314e25bd9c3b112afdd1280d84f458dbdc
vtakaj <vtakaj@gmail.com>
2021-04-26 17:32:05 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <!-- ToDoListを私用する -->
+  <ToDoList />
+</template>
+<script>
+// ToDoList.vueをインポート
+import ToDoList from './components/ToDoList.vue'
+export default {
+  name: 'App',
+  components: {
+    // ToDoListを<template>で使用できるようにする
+    ToDoList
+  }
+}
+</script>
